e7094c85d08b8a86ce3ddd0f14ef570e251601f4
HAYASHI <ayaka.hayashi13@gmail.com>
2025-10-03 15:49:13 +0900

    UIwip

--- Assets/Script/TextMov.cs
+++ Assets/Script/TextMov.cs
@@ -1,33 +1,43 @@
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using UnityEditor.ShaderGraph;
+using System.Collections;
 
 public class TextMov : MonoBehaviour
 {
     public TextMeshProUGUI readyTxt;
     public GameObject Panel;
     public CanvasGroup canPanel;
+    string msg;
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+
+
     void Start()
     {
+        readyTxt = GetComponentInChildren<TextMeshProUGUI>();
+        msg = readyTxt.text;
         FadeIn();
         FadeOut();
     }
     public void FadeIn()
     {
+        
         canPanel.alpha = 0f;
-        canPanel.DOFade(1, 3);
+        canPanel.DOFade(1, 1);
     }
 
 
 
     public void FadeOut()
     {
-        canPanel.alpha = 1f;
-        canPanel.DOFade(0, 3);
-
+     
+        canPanel.alpha = 0.5f;
+        canPanel.DOFade(0, 0.5f);
+       
     }
 }
