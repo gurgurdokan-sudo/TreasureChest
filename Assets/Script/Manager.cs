3dc9055a8a18f8ab3804aec8462da89d74a1a22c
yuya <gurgur.dokan@gmail.com>
2025-10-07 11:51:56 +0900

    manager gameStpe

--- Assets/Script/Manager.cs
+++ Assets/Script/Manager.cs
@@ -5,48 +5,32 @@ using NUnit.Framework;
 using System.Xml.Serialization;
 using UnityEngine.UIElements;
 using DG.Tweening;
-using System.Collections;
-
 
 public class Manager : MonoBehaviour
 {
 
-    public UnityChanContorller unityChanContorller;
+    public Transform unityChanContorller;
+    Vector3 syoki = new Vector3(0, 0.52f, -6.0f);
     public TextMeshProUGUI scoreText;
     public BoxOpen[] boxOpens;
-    public BoxOpen box;
     public ChestTest chestTest;//シャッフル
-
-
-    //仮置き真偽値
-   public bool isCheck=false;
     /*flog　ゲームの進行を制御するための処理　両方False待機、testOKがtrueの時に進行する*/
     public bool testOk = false;//開けた処理後の
     public bool testNg = false;
-
+    enum gameStep
+    {
+        gameStart, witeForPlayerSelct, gameResult ,levelCompeete
+    }
+    gameStep currentGameStep = 0;
     void Start()
     {
         // SetTweensCapacity()
-        scoreText = GetComponent<TextMeshProUGUI>();
-        chestTest = chestTest.GetComponent<ChestTest>();
-       
-        BoxLidMove();
-    }
-
-    public bool maneg()
-    {
-        for (int i = 0; i < boxOpens.Length; i++)
-        {
-            if (boxOpens[i].IsOpen())
-            {
-                return true;
-            }
-        }
-        return false;
+        // scoreText = GetComponent<TextMeshProUGUI>();
+        // chestTest = chestTest.GetComponent<ChestTest>();
     }
     void FullOpen()
     {
-        for (int i = 0; i <boxOpens.Length; i++)
+        for (int i = 0; i < boxOpens.Length; i++)
         {
             boxOpens[i].Open();
         }
@@ -58,50 +42,63 @@ public class Manager : MonoBehaviour
             boxOpens[i].Close();
         }
     }
-
-
-    void BoxLidMove()
+    void SingleLidMove(bool ng)
     {
         Sequence sqe = DOTween.Sequence();
-        sqe.AppendCallback(() => FullOpen());
         sqe.AppendInterval(1.0f);
+        if (ng)
+        {
+            sqe.AppendCallback(() => FullOpen());
+            sqe.AppendInterval(1.0f);
+        }
         sqe.AppendCallback(() => FullClose());
+        sqe.OnComplete(()=> { testNg = false; testOk = false; });
         sqe.Play();
-
-        testNg = false;
-    }
-
-    void SingleLidMove()
-    {
-        Sequence sqe = DOTween.Sequence();
-        sqe.AppendCallback(() => box.Open());
-        sqe.AppendInterval(1.0f);
-        sqe.AppendCallback(() => box.Close());
-        sqe.Play();
-
-        testOk = false;
-        
     }
 
     void Update()
     {
-        if (!testOk && !testNg) return;    //両方選択されずに待機状態
-        else if (testOk && !testNg)//testOKがtureでtestNGがfalseの時(unityちゃんが正解を選んだ時)
+        switch (currentGameStep)
         {
-            //isCheck = box.IsOpen();
-            if (isCheck)
-            {
-                    SingleLidMove();
-            }
-        }  
-        else if (!testOk && testNg) BoxLidMove(); //unityちゃんが不正解を選んだ時
-
-
-
+            case gameStep.gameStart:
+                GameStart();
+                currentGameStep++;
+                break;
+            case gameStep.witeForPlayerSelct:
+                witeForPlayerSelct();
+                break;
+            case gameStep.gameResult:
+                Resule();
+                break;
+            case gameStep.levelCompeete:
+                //すべてのゲームを完了
+                break;
+        }
     }
-
-
-
-
+    void GameStart()
+    {
+        Sequence sqe = DOTween.Sequence();
+        sqe.AppendInterval(2.0f);
+        sqe.AppendCallback(() => FullOpen());
+        sqe.AppendInterval(2.0f);
+        sqe.AppendCallback(() => FullClose());
+        sqe.AppendCallback(() => chestTest.ShuffleRandomSelect());
+        sqe.Play();
+    }
+    void witeForPlayerSelct()
+    {
+        if (!testOk && !testNg) return;    //両方選択されずに待機状態
+        else if (testOk || testNg)//testOKがtureでtestNGがfalseの時(unityちゃんが正解を選んだ時)
+        {
+            SingleLidMove(testNg);
+            currentGameStep++;
+        }
+        // if (testOk) Debug.Log("OK");
+        // else if (testNg); //unityちゃんが不正解を選んだ時
+        }
+    void Resule()
+    {
+        //Levelのカウントアップ/スコア
+    }
 }
 
--- Assets/Script/TextMov.cs
+++ Assets/Script/TextMov.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
-using UnityEditor.ShaderGraph;
-using System.Collections;
 
 public class TextMov : MonoBehaviour
 {
@@ -10,12 +8,6 @@ public class TextMov : MonoBehaviour
     public GameObject Panel;
     public CanvasGroup canPanel;
     string msg;
-
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
-
 
     void Start()
     {
@@ -26,21 +18,12 @@ public class TextMov : MonoBehaviour
     }
     public void FadeIn()
     {
-
         canPanel.alpha = 0f;
         canPanel.DOFade(1, 1);
     }
-
-
-
     public void FadeOut()
     {
-
         canPanel.alpha = 0.5f;
         canPanel.DOFade(0, 1.0f);
-
-    }
-    
-
-    
+    } 
 }
