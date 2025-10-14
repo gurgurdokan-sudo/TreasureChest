c69ce343e22a215b1b8201533557e318e44fff5a
yuya <gurgur.dokan.gmail>
2025-10-14 18:10:01 +0900

    復旧

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+//---------シングルトンクラス------------
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance{ get; private set; }
+    public List<int> ScoreList = new List<int>();
+    int TotalScore = 0;
+
+    public void SetScore(int num)
+    {
+        TotalScore +=num;
+    }
+    
+    public int GetScore()
+    {
+        return this.TotalScore;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // シーンをまたいでも残す
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+      public void ScoreSort(int score)
+    {
+        if (PlayerPrefs.GetInt("HightScore") < score)
+        {
+            PlayerPrefs.SetInt("HightScore", score);
+            ScoreList.Add(score);
+            ScoreList.Sort((a, b) => b.CompareTo(a));
+            // Debug.Log(score + "ハイスコア判定内");
+            // Debug.Log(ScoreList[0]);
+        }
+        else
+        {
+            ScoreList.Add(score);
+            ScoreList.Sort((a, b) => b.CompareTo(a));//sort
+            if (ScoreList.Count > 5)
+            {
+                ScoreList.RemoveRange(5, ScoreList.Count - 5);
+                // Debug.Log(score + "ランキング処理内");
+                // Debug.Log(ScoreList);
+            }
+        }
+    }
+}
