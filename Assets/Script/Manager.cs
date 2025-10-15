d7c8b3dab97e88d166720590e134dd402911caef
yuya <gurgur.dokan.gmail>
2025-10-15 15:47:51 +0900

    ScoreManager

--- Assets/Script/Manager.cs
+++ Assets/Script/Manager.cs
@@ -105,7 +105,7 @@ public class Manager : MonoBehaviour
             if (gameLevle < 3) gameLevle++;
             readyTxt.text = "Great!";
             readyTxt.color = Color.yellow;
-            ScoreManager.Instance.SetScore(score++);
+            ScoreManager.Instance.totalScore+=score;
             SingleLidMove();
             currentGameStep = gameStep.gameRelode;
         }
@@ -132,7 +132,7 @@ public class Manager : MonoBehaviour
         if (life > 0) currentGameStep = gameStep.gameStart;//もう一度ゲームステップ
         else
         {
-            ScoreManager.Instance.ScoreSort(score);
+            ScoreManager.Instance.ScoreSort();
             currentGameStep = gameStep.levelCompeete;
         }
     }
