a63bd4d7f4649bf75f364272d4d71bde2f67e817
HAYASHI <ayaka.hayashi13@gmail.com>
2025-10-15 17:49:28 +0900

    タイトル追加

--- Assets/Script/ResultControl.cs
+++ Assets/Script/ResultControl.cs
@@ -6,7 +6,8 @@ using System.Collections.Generic;
 public class ResultControl : MonoBehaviour
 {
     public TextMeshProUGUI scorePrefab;
-    public TextMeshProUGUI hightScore;
+    public TextMeshProUGUI hightScore; // 命名悪くて申し訳ないですがアタッチするときはHighScoreSet
+    public Transform socoreP;
     
     public void OnStartButtonClicked()
     {
@@ -21,8 +22,12 @@ public class ResultControl : MonoBehaviour
         
         for (int i = 0; i < scores.Count; i++)
         {
-            TextMeshProUGUI scoreObj = Instantiate( scorePrefab,this.transform);
-            scoreObj.text = i + 1+ ":" + scores[i] + "Score";
+            //変えるな
+            //これは自身のTransfomeではなく登録されたゲームオブジェクトのTransfomを参照して
+            // 自身のフォントサイズを自動的に変えるコンポーネントを使用するためのコードなので
+            // 書き換えると文字サイズおかしくなるから変えないでください
+            TextMeshProUGUI scoreObj = Instantiate( scorePrefab,socoreP);
+            scoreObj.text = i + 1+ "位:" + scores[i] + "Score";
         }
     }
 
